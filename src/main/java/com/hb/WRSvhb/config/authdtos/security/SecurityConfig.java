98ff89d5e770c444dc5605d0e7cc1c0ad3fa06d4
24harshal <harshalbhurea1@gmail.com>
2023-09-02 17:08:28 +0530

    add sui

--- src/main/java/com/hb/WRSvhb/config/authdtos/security/SecurityConfig.java
+++ src/main/java/com/hb/WRSvhb/config/authdtos/security/SecurityConfig.java
@@ -71,6 +71,8 @@ public class SecurityConfig {
                         auth.requestMatchers( "/register").permitAll();
                         auth.requestMatchers("/error").permitAll();
                         auth.requestMatchers("/employees/allEmployees").permitAll();
+                        auth.requestMatchers("/swagger-ui/**").permitAll();
+                        auth.requestMatchers("/v3/api-docs/**").permitAll();
                         auth.requestMatchers("/favicon.ico").permitAll();
 //                        auth.anyRequest().permitAll();
                         auth.anyRequest().authenticated();
